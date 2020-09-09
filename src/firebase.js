e676acae838ec4f2d9be0ccaeaf680d074c36444
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-09-09 13:23:31 -0700

    config firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAoV_WXWoxkMlzG6gDkjwuOm390jGEOaWY",
+  authDomain: "whatsapp-mern-666fd.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-666fd.firebaseio.com",
+  projectId: "whatsapp-mern-666fd",
+  storageBucket: "whatsapp-mern-666fd.appspot.com",
+  messagingSenderId: "114273551155",
+  appId: "1:114273551155:web:7ee0cab51c80aa7f61f446",
+  measurementId: "G-XBL6V45FDV",
+};
