a2fdc2c3d7bb78c28dbc8d8d2e88f7793dde559e
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-09-10 11:14:46 -0700

    styled chat messages

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -23,6 +23,13 @@ function Chat() {
           </IconButton>
         </div>
       </div>
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Gurpreet</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
     </div>
   );
 }
