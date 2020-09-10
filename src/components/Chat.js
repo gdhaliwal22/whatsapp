ca2ccc484361d63c8a37c1a7b5b58b6fa69a2106
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-09-10 10:55:53 -0700

    import icons for chat container

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,9 +1,28 @@
 import React from "react";
 import "../css/Chat.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import { SearchOutlined, AttachFile, MoreVert } from "@material-ui/icons";
 function Chat() {
   return (
     <div className="chat">
-      <h1>I am the chat</h1>
+      <div className="chat__header">
+        <Avatar />
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at...</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
