b41625f5ee5a8239340b3183866aa9f0560624df
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-09-10 10:43:54 -0700

    styled search bar in sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,6 +3,7 @@ import "../css/Sidebar.css";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { SearchOutlined } from "@material-ui/icons/";
 
 import { Avatar, IconButton } from "@material-ui/core";
 
@@ -11,17 +12,24 @@ function Sidebar() {
     <div className="sidebar">
       <div className="sidebar__header">
         <Avatar src="https://avatars2.githubusercontent.com/u/47346292?s=460&u=df0ecc71b7849b7165bfe27a4814aeb4c5994ff8&v=4" />
-        <IconButton>
-          <DonutLargeIcon />
-        </IconButton>
-        <IconButton>
-          <ChatIcon />
-        </IconButton>
-        <IconButton>
-          <MoreVertIcon />
-        </IconButton>
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
       </div>
-      <div className="sidebar__headerRight"></div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input placeholder="Search or start new chat" type="text" />
+        </div>
+      </div>
     </div>
   );
 }
