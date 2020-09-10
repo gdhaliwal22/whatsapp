f05bfe338f6c8395bb34a2a5633f2d615862a050
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-09-10 10:29:57 -0700

    import donut, chat, and more vert icons

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,27 @@
 import React from "react";
 import "../css/Sidebar.css";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
+import { Avatar, IconButton } from "@material-ui/core";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="sidebar__header"></div>
+      <div className="sidebar__header">
+        <Avatar src="https://avatars2.githubusercontent.com/u/47346292?s=460&u=df0ecc71b7849b7165bfe27a4814aeb4c5994ff8&v=4" />
+        <IconButton>
+          <DonutLargeIcon />
+        </IconButton>
+        <IconButton>
+          <ChatIcon />
+        </IconButton>
+        <IconButton>
+          <MoreVertIcon />
+        </IconButton>
+      </div>
       <div className="sidebar__headerRight"></div>
-      <DonutLargeIcon />
     </div>
   );
 }
